ebd6c67bf47e080cea9c98e72d63e1c91ec063cd
shantha-123 <k36440670@gmail.com>
2025-08-08 15:23:08 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,24 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const app = express();
-app.use(bodyParser.urlencoded({ extended: true }))
-app.use(bodyParser.json())
-const dbConfig = require('./config/database.config.js');
 const mongoose = require('mongoose');
+const dbConfig = require('./config/database.config.js');
+
+const app = express();
+
+// Middleware
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Connect to MongoDB
 mongoose.Promise = global.Promise;
-mongoose.connect(dbConfig.url, {
-    useNewUrlParser: true
-}).then(() => {
-    console.log("Databse Connected Successfully!!");    
-}).catch(err => {
-    console.log('Could not connect to the database', err);
+mongoose.connect(dbConfig.url)
+  .then(() => {
+    console.log("Database Connected Successfully!!");
+  })
+  .catch(err => {
+    console.error('Could not connect to the database:', err);
     process.exit();
-});
+  });
+
+// Routes
+const UserRoute = require('./app/routes/User');
+app.use('/user', UserRoute);
+
+// Root route
 app.get('/', (req, res) => {
-    res.json({"message": "Hello Crud Node Express"});
+  res.json({ message: "Hello Crud Node Express" });
 });
+
+// Start server
 app.listen(3000, () => {
-    console.log("Server is listening on port 3000");
+  console.log("Server is listening on port 3000");
 });
-const UserRoute = require('./app/routes/User')
-app.use('/user',UserRoute)
